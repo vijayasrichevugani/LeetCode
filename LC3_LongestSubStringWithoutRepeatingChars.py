ce9c601343eabc851b0f5bd540f38a6a4fdd4ad8
vijayasrichevugani <34120632+vijayasrichevugani@users.noreply.github.com>
2022-06-23 01:00:56 -0700

    Update LC3_LongestSubStringWithoutRepeatingChars.py

--- LC3_LongestSubStringWithoutRepeatingChars.py
+++ LC3_LongestSubStringWithoutRepeatingChars.py
@@ -1,1 +1,13 @@
-
+Class Solution:
+  def __init__self(self, s:str) -> int:
+    Char_set = set()
+    res = 0
+    l=0
+    for r in range(len(s)):
+      while s[r] in Char_set:
+        Char_set.remove(s[l])
+        l+=1
+      Char_set.add(s[r])
+      res = max(res,r+l-1)
+      
+    return res
