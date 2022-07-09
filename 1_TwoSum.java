18aa9f4e1fef6d6c9467964c36954d042fe840fb
vijayasrichevugani <34120632+vijayasrichevugani@users.noreply.github.com>
2022-07-09 03:58:40 -0700

    Create 1_TwoSum.java

---
+++ 1_TwoSum.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int[] op = new int[2];
+        for(int i=0;i<nums.length;i++){
+            for(int j=i+1;j<nums.length;j++){
+                int sum = nums[i]+nums[j];
+                if(sum == target){
+                    op[0] = i;
+                    op[1] = j;
+                    break;
+                }
+                else{continue;}
+            }
+        }
+        return op;
+    }
+}
